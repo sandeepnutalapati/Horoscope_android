eb60af8a05090347524b6f7e6e570dfe8c8e058d
Harshit55 <harshitkml@gmail.com>
2018-02-25 23:01:16 +0530

    initial page with circles

--- app/src/main/java/com/example/sandeep/horoscope/MainActivity.java
+++ app/src/main/java/com/example/sandeep/horoscope/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -13,18 +14,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton button1 = findViewById(R.id.imageButton1);
-        ImageButton button2 = findViewById(R.id.imageButton2);
-        ImageButton button3 = findViewById(R.id.imageButton3);
-        ImageButton button4 = findViewById(R.id.imageButton4);
-        ImageButton button5 = findViewById(R.id.imageButton5);
-        ImageButton button6 = findViewById(R.id.imageButton6);
-        ImageButton button7 = findViewById(R.id.imageButton7);
-        ImageButton button8 = findViewById(R.id.imageButton8);
-        ImageButton button9 = findViewById(R.id.imageButton9);
-        ImageButton button10 = findViewById(R.id.imageButton10);
-        ImageButton button11 = findViewById(R.id.imageButton11);
-        ImageButton button12= findViewById(R.id.imageButton12);
+        ImageView button1 = findViewById(R.id.aries);
+        ImageView button2 = findViewById(R.id.taurus);
+        ImageView button3 = findViewById(R.id.gemini);
+        ImageView button4 = findViewById(R.id.cancer);
+        ImageView button5 = findViewById(R.id.leo);
+        ImageView button6 = findViewById(R.id.virgo);
+        ImageView button7 = findViewById(R.id.libra);
+        ImageView button8 = findViewById(R.id.scorpio);
+        ImageView button9 = findViewById(R.id.sagittarius);
+        ImageView button10 = findViewById(R.id.capricorn);
+        ImageView button11 = findViewById(R.id.acquarius);
+        ImageView button12= findViewById(R.id.pisces);
 
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
@@ -49,62 +50,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent = new Intent(this,HoroscopeActivity.class);
         switch (v.getId()) {
 
-            case R.id.imageButton1:
+            case R.id.aries:
                 intent.putExtra("sign",1);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton2:
+            case R.id.taurus:
                 intent.putExtra("sign",2);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton3:
+            case R.id.gemini:
                 intent.putExtra("sign",3);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton4:
+            case R.id.cancer:
                 intent.putExtra("sign",4);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton5:
+            case R.id.leo:
                 intent.putExtra("sign",5);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton6:
+            case R.id.virgo:
                 intent.putExtra("sign",6);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton7:
+            case R.id.libra:
                 intent.putExtra("sign",7);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton8:
+            case R.id.scorpio:
                 intent.putExtra("sign",8);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton9:
+            case R.id.sagittarius:
                 intent.putExtra("sign",9);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton10:
+            case R.id.capricorn:
                 intent.putExtra("sign",10);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton11:
+            case R.id.acquarius:
                 intent.putExtra("sign",11);
                 startActivity(intent);
                 break;
 
-            case R.id.imageButton12:
+            case R.id.pisces:
                 intent.putExtra("sign",12);
                 startActivity(intent);
                 break;
